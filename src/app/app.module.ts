b238977a5b36aa9f83a92bfac2bb9b1da4d185f7
askargillani <your-email@example.com>
2023-10-03 00:29:10 +0500

    header made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WebHeaderComponent } from './web-header/web-header.component';
+import { WebHeaderComponent } from './shared/web-header/web-header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { WebHeaderComponent } from './web-header/web-header.component';
     WebHeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
