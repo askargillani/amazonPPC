979b10d505de67a86cd05ed23130cad33d61a51e
askargillani <your-email@example.com>
2023-10-07 15:29:12 +0500

    angular materialui installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WebHeaderComponent } from './shared/web-header/web-header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button'; 
+import { WebHeaderComponent } from './shared/web-header/web-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebHeaderComponent,
+    WebHeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
