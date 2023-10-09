26d295f6dc0e95bf6cd50653bfd8bcf0c5cee2e9
askargillani <your-email@example.com>
2023-10-10 00:42:00 +0500

    cards implemented
    cards implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WebHeaderComponent } from './shared/web-header/web-header.component';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import {MatIconModule} from '@angular/material/icon'
+import { DisplayCardComponent } from './shared/display-card/display-card.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebHeaderComponent
+    WebHeaderComponent,
+    DisplayCardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
